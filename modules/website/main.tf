fd3a334af41acf56c721583ab8d646b350b31bdb
郑思龙 <zhengsilong@zhengsilongdeMacBook-Pro.local>
2020-07-14 22:08:00 +0800

    make website files resource depend on s3 resource

--- modules/website/main.tf
+++ modules/website/main.tf
@@ -28,11 +28,13 @@ POLICY
   websites = var.websites
 }
 
-# https://stackoverflow.com/questions/57456167/uploading-multiple-files-in-aws-s3-from-terraform
 resource "aws_s3_bucket_object" "object" {
+  # https://stackoverflow.com/questions/57456167/uploading-multiple-files-in-aws-s3-from-terraform
   for_each = fileset(var.static_web_site_folder, "*")
   bucket   = var.bucket_name
   key      = each.value
   source   = "${var.static_web_site_folder}${each.value}"
   etag     = filemd5("${var.static_web_site_folder}${each.value}")
+  # https://stackoverflow.com/questions/58275233/terraform-depends-on-with-modules
+  depends_on = [module.website]
 }
