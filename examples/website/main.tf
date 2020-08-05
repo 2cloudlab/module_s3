9c4240cc3a5258958e469a123d1a991a97c3aaa8
slz <slz@slzs-MacBook-Pro.local>
2020-08-05 23:35:37 +0800

    format comment

--- examples/website/main.tf
+++ examples/website/main.tf
@@ -12,7 +12,9 @@ module "website" {
   bucket_name            = "2cloudlab.example.com"
   acl                    = "public-read"
   static_web_site_folder = "test_website/"
-  # In order to restrict users directly access to your static website by s3's website_endpoint, uncomment below command after created cloudfront, then specific the generated origin_access_identity arn and rerun this code
+  # In order to restrict users directly access to your static website by s3's website_endpoint,
+  # uncomment below command after created cloudfront,
+  # then specific the generated origin_access_identity arn and re-run this code
   # origin_access_identity_arn = "arn:aws:iam::cloudfront:user/CloudFront Origin Access Identity E3PK5Q3TDLDPJC"
 }
 
